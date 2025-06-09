f590adf5bfabeb48ea0b453016fee54e5ee0f712
Denis Kostiuk <64838929+Chrumba@users.noreply.github.com>
2025-06-09 17:26:32 +0300

    History drawer profile view and history fixes

--- app/src/main/java/com/savana/domain/usecases/history/GetHistoryUseCase.kt
+++ app/src/main/java/com/savana/domain/usecases/history/GetHistoryUseCase.kt
@@ -6,32 +6,59 @@ import kotlinx.coroutines.delay
 
 class GetHistoryUseCase {
 
-    private val historyPlaceholder = HistoryEntry(
+    private val historySuccessPlaceholder = HistoryEntry(
         id = 1,
         label = "Abpba 1",
         status = Status.Success
+    )
+
+    private val historyAnalyzingPlaceholder = HistoryEntry(
+        id = 1,
+        label = "Abpba 1",
+        status = Status.Analyzing
+    )
+
+    private val historyDenyPlaceholder = HistoryEntry(
+        id = 1,
+        label = "Abpba 1",
+        status = Status.Deny
     )
 
     suspend operator fun invoke(): List<HistoryEntry>{
         delay(1000)
         return buildList {
 
-            add(historyPlaceholder)
-            add(historyPlaceholder)
-            add(historyPlaceholder)
-            add(historyPlaceholder)
-            add(historyPlaceholder)
-            add(historyPlaceholder)
-            add(historyPlaceholder)
-            add(historyPlaceholder)
-            add(historyPlaceholder)
+            add(historyAnalyzingPlaceholder)
+            add(historySuccessPlaceholder)
+            add(historyDenyPlaceholder)
+            add(historyAnalyzingPlaceholder)
+            add(historySuccessPlaceholder)
+            add(historyDenyPlaceholder)
+            add(historyAnalyzingPlaceholder)
+            add(historySuccessPlaceholder)
+            add(historyDenyPlaceholder)
+            add(historyAnalyzingPlaceholder)
+            add(historySuccessPlaceholder)
+            add(historyDenyPlaceholder)
+            add(historyAnalyzingPlaceholder)
+            add(historySuccessPlaceholder)
+            add(historyDenyPlaceholder)
+            add(historyAnalyzingPlaceholder)
+            add(historySuccessPlaceholder)
+            add(historyDenyPlaceholder)
+            add(historyAnalyzingPlaceholder)
+            add(historySuccessPlaceholder)
+            add(historyDenyPlaceholder)
+            add(historyAnalyzingPlaceholder)
+            add(historySuccessPlaceholder)
+            add(historyDenyPlaceholder)
         }
     }
 
     suspend operator fun invoke(historyId: Int): HistoryEntry{
         delay(1000)
 
-        return historyPlaceholder
+        return historyAnalyzingPlaceholder
     }
 
 }
